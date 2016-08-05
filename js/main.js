c59271cef3d940fbdbdfcd6cbf8aa0c00b378da9
diegopenhanut <diegopenhanut@gmail.com>
2016-08-05 14:37:31 -0300

    update app for manuscript submission

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 window.onload = function() {
 		var fileInput = document.getElementById('fileInput');
-		//var fileDisplayArea = document.getElementById('fileDisplayArea');
 
 		fileInput.addEventListener('change', function(e) {
 			var file = fileInput.files[0];
@@ -12,34 +11,40 @@ window.onload = function() {
 				reader.onload = function(e) {
                                     //load vcf object as global
                                     vcf = parse_vcf(reader.result)
-                                    //create button obj
-                                    btn = document.createElement("button");
-                                    btn.innerText = "Send JsonLd to Console";
-                                    btn.className = "btn btn-default btn-file";
-                                    btn.onclick = function(){
-                                        vcf2jsonLd(vcf, function(a){vcfJsonLd = a})
-                                    };
                                     
-                                    document.getElementById("secondaryButtons").appendChild(btn);
+                                        vcf2jsonLd(vcf, function(a){vcfJsonLd = a});
 				    
                                     console.log("vcf object avaliable")
                                     
                                     vcf2rdf(reader.result, function(res){
-                                        var blob = URL.createObjectURL(new Blob([res]));
-                                            a = document.createElement("a");
-                                            a.href = blob;
-                                            a.className = "btn btn-default btn-file";
+					var innerJson = JSON.stringify(vcfJsonLd, null, 3)
+                                        var blobNquad = URL.createObjectURL(new Blob([res]));
+                                        var blobJSON = URL.createObjectURL(new Blob([innerJson]));
+                                            
+					    a = document.createElement("a");
+                                            a.href = blobNquad;
+                                            a.className = "btn btn-default btn-file col-md-6";
                                             a.download = 'myquad.nq';
-                                            a.innerText = 'Save output';
+                                            a.innerText = 'Save Nquad output';
                                             document.getElementById("secondaryButtons").appendChild( a );
-                                            fileDisplayArea.innerText = res;
+
+					    b = document.createElement("a");
+                                            b.href = blobJSON;
+                                            b.className = "btn btn-default btn-file col-md-6";
+                                            b.download = 'vcfJsonLd.json';
+                                            b.innerText = 'Save JSON output';
+                                            document.getElementById("secondaryButtons").appendChild( b );
+
+                                            nquadsDisplayArea.innerText = res;
+                                            jsonDisplayArea.innerText = innerJson;;
 					})
 
 				}
 
 				reader.readAsText(file);	
 			} else {
-				fileDisplayArea.innerText = "File not supported!"
+				nquadsDisplayArea.innerText = "File not supported!"
+				jsonDisplayArea.innerText = "File not supported!"
 			}
 		});
 }
